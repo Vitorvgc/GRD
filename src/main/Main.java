1837157969d77f1dc3919b9a3346728dc65f757e
Edvaldo Junior <dujunior1300@gmail.com>
2018-10-30 10:46:03 -0300

    feat(views): add basic layout of main screen

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../views/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../views/main.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 450));
         primaryStage.show();
     }
 
