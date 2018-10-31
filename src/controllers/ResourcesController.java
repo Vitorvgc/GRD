80212095e76dc18d6094b45012454ab621ab83c7
Vitorvgc <vitorvgc07@gmail.com>
2018-10-31 01:57:55 -0300

    feat(controllers): show resource on double click in table

--- src/controllers/ResourcesController.java
+++ src/controllers/ResourcesController.java
@@ -4,9 +4,15 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 import models.managers.DataManager;
 import models.resource.Resource;
 
@@ -28,6 +34,22 @@ public class ResourcesController {
         System.out.println("Add resource");
     }
 
+    private void showResource(Resource resource) {
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../views/resource.fxml"));
+            ResourceController controller = new ResourceController(resource);
+            loader.setController(controller);
+            Pane pane = loader.load();
+            Stage stage = new Stage();
+            stage.setTitle(resource.getName());
+            stage.setScene(new Scene(pane));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     private void setupTable() {
 
         TableColumn<Resource, String> nameColumn = (TableColumn) resourcesTable.getColumns().get(0);
@@ -40,7 +62,20 @@ public class ResourcesController {
         sectionColumn.setCellValueFactory(cellData -> new SimpleStringProperty(Integer.toString(cellData.getValue().getSection())));
         ocurrencesColumn.setCellValueFactory(cellData -> new SimpleStringProperty(Integer.toString(cellData.getValue().getOcurrences().size())));
 
+        // double click event on row -> show clicked resource
+        resourcesTable.setRowFactory(tableView -> {
+            TableRow<Resource> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if(event.getButton() == MouseButton.PRIMARY && event.getClickCount() == 2) {
+                    Resource resource = row.getItem();
+                    showResource(resource);
+                }
+            });
+            return row;
+        });
+
+        resourcesTable.setPlaceholder(new Label("Nenhum recurso registrado"));
         resourcesTable.setItems(data);
-        resourcesTable.setPlaceholder(new Label("No resources registered"));
+
     }
 }
