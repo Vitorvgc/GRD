8da4ce2e86dc9bc902f0a5d1c7b33f53ff9aa5df
Edvaldo Junior <dujunior1300@gmail.com>
2018-11-12 17:13:58 -0300

    feat(controllers): display info of model on model screen

---
+++ src/controllers/ModelController.java
@@ -0,0 +1,95 @@
+package controllers;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import models.managers.DataManager;
+import models.resource.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ModelController {
+
+    @FXML
+    private Label modelNameLabel;
+
+    @FXML
+    private ToggleGroup contentTabs;
+
+    @FXML
+    private ToggleButton parametersToggleButton,
+                         occurrencesToggleButton;
+
+    @FXML
+    private ListView<String> occurrencesList;
+
+    @FXML
+    private TableView< Map.Entry<String, Class> > parametersTable;
+
+    @FXML
+    private Pane contentContainer;
+
+    private Model model;
+
+    public ModelController(Model model) {
+        this.model = model;
+    }
+
+    @FXML
+    private void initialize() {
+
+        setupTitle();
+        setupToggleButtons();
+        setupParametersTable();
+        setupOcurrencesList();
+    }
+
+    private void setupTitle() {
+        modelNameLabel.setText(model.getName());
+    }
+
+    private void setupToggleButtons() {
+
+        contentTabs.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+
+            if(newValue == null)
+                oldValue.setSelected(true);
+            else if(parametersToggleButton.isSelected()) {
+                parametersTable.setVisible(true);
+                occurrencesList.setVisible(false);
+            }
+            else if(occurrencesToggleButton.isSelected()) {
+                parametersTable.setVisible(false);
+                occurrencesList.setVisible(true);
+            }
+        });
+    }
+
+    private void setupParametersTable() {
+
+        TableColumn< Map.Entry<String, Class>, String > fieldColumn = (TableColumn) parametersTable.getColumns().get(0);
+        TableColumn< Map.Entry<String, Class>, String > typeColumn = (TableColumn) parametersTable.getColumns().get(1);
+
+        fieldColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getKey()));
+        typeColumn.setCellValueFactory(cellData -> new SimpleStringProperty(DataManager.getInstance().getNameOfClass(cellData.getValue().getValue())));
+
+        ArrayList< Map.Entry<String, Class> > parameter = new ArrayList<>(model.getParameters().entrySet());
+        parametersTable.setItems(FXCollections.observableArrayList(parameter));
+    }
+
+    private void setupOcurrencesList() {
+
+        occurrencesList.setPlaceholder(new Label("Nenhuma ocorrência registrada"));
+
+        List<String> occurrences = model.getOccurrenceTypes().stream()
+                .map(type -> type.getTitle())
+                .collect(Collectors.toList());
+
+        occurrencesList.setItems(FXCollections.observableArrayList(new ArrayList<>(occurrences)));
+    }
+}
